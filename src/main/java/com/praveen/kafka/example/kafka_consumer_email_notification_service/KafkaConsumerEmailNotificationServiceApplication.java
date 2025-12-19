dca9d8f8fc42af7ec6bb4550b2d1f4be6e6b72b6
Praveen Kithani <praveenk.kithani@gmail.com>
2025-12-19 13:58:34 +0530

    initial project structure with application.properties files for kafka consumer service

---
+++ src/main/java/com/praveen/kafka/example/kafka_consumer_email_notification_service/KafkaConsumerEmailNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.kafka.example.kafka_consumer_email_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerEmailNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerEmailNotificationServiceApplication.class, args);
+	}
+
+}
